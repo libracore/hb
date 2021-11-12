83fd85ee513b27dad81c4b0cca62d0ccbddf88d5
lasalesi <larsericmueller@gmail.com>
2021-11-12 20:07:08 +0100

    email footer signature

--- heimbohrtechnik/heim_bohrtechnik/doctype/signature/signature.py
+++ heimbohrtechnik/heim_bohrtechnik/doctype/signature/signature.py
@@ -3,8 +3,11 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
 
 class Signature(Document):
-	pass
+    def before_save(self):
+        email_footer = html = frappe.render_template("heimbohrtechnik/templates/includes/email_footer.html", self.as_dict())
+        self.email_footer = email_footer
+        return
