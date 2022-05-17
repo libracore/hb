a9ce9b0c8b31f94f8989615d2f1494f05d7f35c3
lasalesi <larsericmueller@gmail.com>
2022-05-17 16:39:32 +0200

    hotfix translation issue

--- heimbohrtechnik/heim_bohrtechnik/page/object_overview/object_overview.js
+++ heimbohrtechnik/heim_bohrtechnik/page/object_overview/object_overview.js
@@ -1,7 +1,7 @@
 frappe.pages['object-overview'].on_page_load = function(wrapper) {
     var page = frappe.ui.make_app_page({
         parent: wrapper,
-        title: 'Object Overview',
+        title: __('Object Overview'),
         single_column: true
     });
     
