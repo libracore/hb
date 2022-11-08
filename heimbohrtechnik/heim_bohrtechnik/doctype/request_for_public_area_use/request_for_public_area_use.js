83426f34e19cc31b6e359e7ca30047809fde4e33
lasalesi <larsericmueller@gmail.com>
2022-11-08 10:16:10 +0100

    allow to pull address for road blocks from owner or customer

--- heimbohrtechnik/heim_bohrtechnik/doctype/request_for_public_area_use/request_for_public_area_use.js
+++ heimbohrtechnik/heim_bohrtechnik/doctype/request_for_public_area_use/request_for_public_area_use.js
@@ -4,12 +4,19 @@
 frappe.ui.form.on('Request for Public Area Use', {
     object: function(frm) {
         if (frm.doc.object) {
-            fetch_object_details(frm.doc.object);
+            fetch_object_details(frm.doc.object, "Auftraggeber");
+            fetch_project_details(frm.doc.object);
         }
+    },
+    btn_from_customer(frm) {
+        fetch_object_details(frm.doc.object, "Auftraggeber");
+    },
+    btn_from_owner(frm) {
+        fetch_object_details(frm.doc.object, "Eigentümer");
     }
 });
 
-function fetch_object_details(obj) {
+function fetch_object_details(obj, address_type) {
     frappe.call({
         "method": "frappe.client.get",
         "args": {
@@ -21,27 +28,12 @@ function fetch_object_details(obj) {
             cur_frm.set_value("object_name", object.object_name);
             cur_frm.set_value("object_street", object.object_street);
             cur_frm.set_value("object_location", object.object_location);
-            for (var a = 0; a < object.addresses.length; a++) {
-                if (object.addresses[a].address_type === "Eigentümer") {
-                    if (object.addresses[a].is_simple) {
-                        cur_frm.set_value("address_name", object.addresses[a].simple_name);
-                        var address_parts = object.addresses[a].simple_address.split(", ");
-                        cur_frm.set_value("address_street", address_parts[0]);
-                        if (address_parts.length > 1) {
-                            cur_frm.set_value("address_location", address_parts[1]);
-                        }
-                    } else {
-                        var address_parts = object.addresses[a].address_display.split("<br>");
-                        cur_frm.set_value("address_name", object.addresses[a].party_name);
-                        if (address_parts.length > 2) {
-                            cur_frm.set_value("address_street", address_parts[0]);
-                            cur_frm.set_value("address_location", address_parts[1]);
-                        }
-                    }
-                }
-            }
+            fetch_address_from(object, address_type);
         }
     });
+}
+
+function fetch_project_details(obj) {
     frappe.call({
         "method": "frappe.client.get",
         "args": {
@@ -54,4 +46,26 @@ function fetch_object_details(obj) {
             cur_frm.set_value("to_date", project.expected_end_date);
         }
     });
+}
+
+function fetch_address_from(object, address_type) {
+    for (var a = 0; a < object.addresses.length; a++) {
+        if (object.addresses[a].address_type === address_type) {
+            if (object.addresses[a].is_simple) {
+                cur_frm.set_value("address_name", object.addresses[a].simple_name);
+                var address_parts = object.addresses[a].simple_address.split(", ");
+                cur_frm.set_value("address_street", address_parts[0]);
+                if (address_parts.length > 1) {
+                    cur_frm.set_value("address_location", address_parts[1]);
+                }
+            } else {
+                var address_parts = object.addresses[a].address_display.split("<br>");
+                cur_frm.set_value("address_name", object.addresses[a].party_name);
+                if (address_parts.length > 2) {
+                    cur_frm.set_value("address_street", address_parts[1]);
+                    cur_frm.set_value("address_location", address_parts[2]);
+                }
+            }
+        }
+    }
 }
