5379b9ff5256b956a9b6e93782277f49d1127fea
lasalesi <larsericmueller@gmail.com>
2023-05-24 22:11:52 +0200

    sales pipeline

---
+++ heimbohrtechnik/heim_bohrtechnik/report/sales_pipeline/sales_pipeline.js
@@ -0,0 +1,9 @@
+// Copyright (c) 2023, libracore AG and contributors
+// For license information, please see license.txt
+/* eslint-disable */
+
+frappe.query_reports["Sales Pipeline"] = {
+    "filters": [
+
+    ]
+};
