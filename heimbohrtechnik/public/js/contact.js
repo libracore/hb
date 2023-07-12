8a7d2b4003ee011d7f7654317c4b553a9750e67c
lasalesi <larsericmueller@gmail.com>
2023-07-12 17:36:52 +0200

    Update contact.js
    use core function to fetch doctype record

--- heimbohrtechnik/public/js/contact.js
+++ heimbohrtechnik/public/js/contact.js
@@ -3,8 +3,7 @@ frappe.ui.form.on('Contact', {
         // compile full name
         cur_frm.set_value("full_name", (frm.doc.first_name || "") + " " + (frm.doc.last_name || ""));
         // if hotel, set phone as primary
-        'async'
-        let primary = false;
+        var primary = false;
         if (frm.doc.phone_nos.length > 0) {
             for (const number of frm.doc.phone_nos) {
                 if (number.is_primary_phone == 1) {
@@ -13,14 +12,16 @@ frappe.ui.form.on('Contact', {
             }
             if (primary == false) {
                 frappe.call({
-                    'method': 'heimbohrtechnik.heim_bohrtechnik.contact.get_supplier',
+                    'method': 'frappe.client.get',
                     'args': {
-                        'supplier': frm.doc.links[0].link_name
+                        'doctype': "Supplier"
+                        'name': frm.doc.links[0].link_name
                     },
+                    'async': false,
                     'callback': function(response) {
-                        var supplier_dict = response.message;
+                        var supplier = response.message;
                         var link = frm.doc.name;
-                        if (supplier_dict.supplier_group == "Hotel") {
+                        if (supplier.supplier_group == "Hotel") {
                             frappe.model.set_value(cur_frm.doc.phone_nos[0].doctype, cur_frm.doc.phone_nos[0].name, "is_primary_phone", 1);
                         }
                     }
