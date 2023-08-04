9c7c38772e9dd3cee96b607a2ea05870958ea0db
lasalesi <larsericmueller@gmail.com>
2023-08-04 11:22:40 +0200

    hotfix contact saving does not work

--- heimbohrtechnik/public/js/contact.js
+++ heimbohrtechnik/public/js/contact.js
@@ -2,30 +2,16 @@ frappe.ui.form.on('Contact', {
     before_save(frm) {
         // compile full name
         cur_frm.set_value("full_name", (frm.doc.first_name || "") + " " + (frm.doc.last_name || ""));
-        // if hotel, set phone as primary
-        var primary = false;
-        if (frm.doc.phone_nos.length > 0) {
-            for (const number of frm.doc.phone_nos) {
-                if (number.is_primary_phone == 1) {
-                    primary = true;
+        // set first phone number as primary
+        var has_primary = false;
+        if ((frm.doc.phone_nos) && (frm.doc.phone_nos.length > 0)) {
+            for (var i = 0; i < frm.doc.phone_nos.length; i++) {
+                if (frm.doc.phone_nos[i].is_primary_phone === 1) {
+                    has_primary = true;
                 }
             }
-            if (primary == false) {
-                frappe.call({
-                    'method': 'frappe.client.get',
-                    'args': {
-                        'doctype': "Supplier",
-                        'name': frm.doc.links[0].link_name
-                    },
-                    'async': false,
-                    'callback': function(response) {
-                        var supplier = response.message;
-                        var link = frm.doc.name;
-                        if (supplier.supplier_group == "Hotel") {
-                            frappe.model.set_value(cur_frm.doc.phone_nos[0].doctype, cur_frm.doc.phone_nos[0].name, "is_primary_phone", 1);
-                        }
-                    }
-                });
+            if (has_primary === false) {
+                frappe.model.set_value(cur_frm.doc.phone_nos[0].doctype, cur_frm.doc.phone_nos[0].name, "is_primary_phone", 1);
             }
         }
     }
