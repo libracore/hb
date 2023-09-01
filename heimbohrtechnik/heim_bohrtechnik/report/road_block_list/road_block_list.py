57b98f67ef58f898e1b64f5fc6c2d85c2866c2f1
Ivanios <ivan.lochbihler@libracore.com>
2023-09-01 09:04:16 +0200

    strassensperrung

--- heimbohrtechnik/heim_bohrtechnik/report/road_block_list/road_block_list.py
+++ heimbohrtechnik/heim_bohrtechnik/report/road_block_list/road_block_list.py
@@ -13,29 +13,28 @@ def execute(filters=None):
 def get_columns(filters):
     return [
         {"label": _("Projekt"), "fieldname": "project", "fieldtype": "Link", "options": "Project", "width": 85},
-        {"label": _("Objekt"), "fieldname": "project", "fieldtype": "Link", "options": "Object", "width": 75},
         {"label": _("Auftrag"), "fieldname": "sales_order", "fieldtype": "Link", "options": "Sales Order", "width": 90},
         {"label": _("Strassensperrung"), "fieldname": "road_block", "fieldtype": "Link", "options": "Request for Public Area Use", "width": 90},
         {"label": _("Bohrteam"), "fieldname": "drilling_team", "fieldtype": "Link", "options": "Drilling Team", "width": 150},
         {"label": _("Objektname"), "fieldname": "object_name", "fieldtype": "Data", "width": 150},
-        {"label": _("Objektname"), "fieldname": "object_name", "fieldtype": "Data", "width": 150},
+        {"label": _("Strasse"), "fieldname": "street", "fieldtype": "Data", "width": 150},
+        {"label": _("Ortschaft"), "fieldname": "location", "fieldtype": "Data", "width": 150},
         {"label": _("Bohrdatum Start"), "fieldname": "drilling_from_date", "fieldtype": "Date", "width": 90},
         {"label": _("Bohrdatum Ende"), "fieldname": "drilling_to_date", "fieldtype": "Date", "width": 90},
         {"label": _("Strassensperrung Start"), "fieldname": "road_block_from_date", "fieldtype": "Data", "width": 90},
-        {"label": _("Strassensperrung Ende"), "fieldname": "road_block_to_date", "fieldtype": "Data", "width": 90},
-        {"label": _("Bewilligungsnr."), "fieldname": "permit_no", "fieldtype": "Data", "width": 90},
-        {"label": _("Bewilligungsdatum"), "fieldname": "permit_date", "fieldtype": "Date", "width": 90}
+        {"label": _("Strassensperrung Ende"), "fieldname": "road_block_to_date", "fieldtype": "Data", "width": 90}
     ]
 
 def get_data(filters):
     # get all projects that have a road block in their checklist
     sql_query = """SELECT
             `tabProject`.`name` AS `project`,
-            `tabObject`.`name` AS `object`,
             `tabProject`.`sales_order` AS `sales_order`,
             `tabRequest for Public Area Use`.`name` AS `road_block`,
             `tabProject`.`drilling_team` AS `drilling_team`,
             `tabObject`.`object_name` AS `object_name`,
+            `tabObject`.`object_street` AS `street`,
+            `tabObject`.`object_location` AS `location`,
             `tabProject`.`expected_start_date` AS `drilling_from_date`,
             `tabProject`.`expected_end_date` AS `drilling_to_date`,
             `tabRequest for Public Area Use`.`from_date` AS `road_block_from_date`,
