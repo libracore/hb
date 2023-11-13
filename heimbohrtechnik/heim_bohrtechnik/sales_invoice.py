5cbad1b5d3487d3d433dd90aac8650484fe3f770
Ivanios <ivan.lochbihler@libracore.com>
2023-11-13 14:49:26 +0100

    zs

--- heimbohrtechnik/heim_bohrtechnik/sales_invoice.py
+++ heimbohrtechnik/heim_bohrtechnik/sales_invoice.py
@@ -5,38 +5,29 @@ import frappe
 from frappe import _
 
 @frappe.whitelist()    
-def validate_prices(invoice_name, objekt):
+def validate_prices(invoice_name):
     
-    #get all items of invoice
-    items_query = """SELECT
+    #get all items which have different prices
+    items = frappe.db.sql("""SELECT
     `tabSales Invoice Item`.`item_code`, 
     `tabSales Invoice Item`.`item_name`,
-    `tabSales Order Item`.`rate`, 
-    `tabSales Invoice Item`.`rate`,
-    `tabSales Order Item`.`parent`
+    `tabSales Invoice Item`.`sales_order`,
+    `tabSales Order Item`.`rate`
     FROM `tabSales Invoice Item` 
     LEFT JOIN `tabSales Order Item` ON `tabSales Order Item`.`name` = `tabSales Invoice Item`.`so_detail` 
     WHERE `tabSales Invoice Item`.`parent` = '{sales_invoice}'
     AND `tabSales Order Item`.`rate` != `tabSales Invoice Item`.`rate`;
-    """.format(sales_invoice=invoice_name)
-    items = frappe.db.sql(items_query, as_dict=True)
+    """.format(sales_invoice=invoice_name), as_dict=True)
     
-    if len(sales_order_data) == 0:
-        return None
     frappe.log_error(items, "items")
-    sales_order = sales_order_data[0]['name']
     
-    item_codes = []
-    item_names = []
+    # ~ #get Sales Order name to display
+    # ~ sales_order = frappe.db.sql("""SELECT
+    # ~ `tabSales Order Item`.`parent`
+    # ~ FROM `tabSales Invoice Item` 
+    # ~ LEFT JOIN `tabSales Order Item` ON `tabSales Order Item`.`name` = `tabSales Invoice Item`.`so_detail` 
+    # ~ WHERE `tabSales Invoice Item`.`parent` = '{sales_invoice}'
+    # ~ LIMIT 1;
+    # ~ """.format(sales_invoice=invoice_name), as_dict=True)
     
-    for invoice_item in invoice_data:
-        for so_item in sales_order_data:
-            if invoice_item['item_code'] == so_item['item_code'] and invoice_item['rate'] != so_item['rate']:
-                if not invoice_item['item_code'] in item_codes:
-                    item_codes.append(invoice_item['item_code'])
-                    item_names.append(invoice_item['item_name'])
-    
-    return item_codes, item_names, sales_order
-
-
-SELECT `tabSales Order Item`.`rate`, `tabSales Invoice Item`.`rate` FROM `tabSales Invoice Item` LEFT JOIN `tabSales Order Item` ON `tabSales Order Item`.`name` = `tabSales Invoice Item`.`so_detail` WHERE `tabSales Invoice Item`.`parent` = "{sales_invoice}";
+    return items
