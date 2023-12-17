6644a636a9b1e18e5ac18f46b7ab674dc531f3b1
lasalesi <larsericmueller@gmail.com>
2023-12-17 21:15:48 +0100

    expension of expense workflow

--- heimbohrtechnik/heim_bohrtechnik/doctype/expense_receipt/expense_receipt.js
+++ heimbohrtechnik/heim_bohrtechnik/doctype/expense_receipt/expense_receipt.js
@@ -1,9 +1,26 @@
 // Copyright (c) 2023, libracore AG and contributors
 // For license information, please see license.txt
 
+// filter for accounts
+cur_frm.fields_dict['expense_account'].get_query = function(doc) {
+     return {
+         filters: {
+             "company": frappe.defaults.get_user_default("company") || frappe.defaults.get_global_default("company")
+         }
+     }
+}
+cur_frm.fields_dict['vat_account'].get_query = function(doc) {
+     return {
+         filters: {
+             "company": frappe.defaults.get_user_default("company") || frappe.defaults.get_global_default("company")
+         }
+     }
+}
+    
 frappe.ui.form.on('Expense Receipt', {
     refresh: function(frm) {
         if (frm.doc.__islocal) {
+            cur_frm.set_value("journal_entry", null);
             cur_frm.set_value("date", new Date());
             frappe.call({
                 'method': "frappe.client.get_list",
@@ -20,6 +37,19 @@ frappe.ui.form.on('Expense Receipt', {
                     if (employee.length > 0) {
                         cur_frm.set_value("employee", employee[0].name);
                     }
+                }
+            });
+            frappe.call({
+                'method': "frappe.client.get",
+                'args': {
+                    'doctype': "Heim Settings",
+                    'name': "Heim Settings"
+                },
+                'callback': function(response) {
+                    var settings = response.message;
+                    
+                    cur_frm.set_value("expense_account", settings.default_expense_account);
+                    cur_frm.set_value("vat_account", settings.vat_account);
                 }
             });
         }
--- heimbohrtechnik/heim_bohrtechnik/doctype/expense_receipt/expense_receipt.py
+++ heimbohrtechnik/heim_bohrtechnik/doctype/expense_receipt/expense_receipt.py
@@ -3,8 +3,59 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
 from frappe.model.document import Document
+from erpnext import get_default_company
 
 class ExpenseReceipt(Document):
-	pass
+        
+    def on_submit(self):
+        # create journal entry
+        multi_currency = 0
+        if self.currency != "CHF":
+            multi_currency = 1
+            
+        jv = frappe.get_doc({
+            'doctype': 'Journal Entry',
+            'multi_currency': multi_currency,
+            'posting_date': self.date,
+            'user_remark': "{0} ({1})".format(
+                self.remarks or "Spesen {0}".format(self.employee_name),
+                self.name),
+            'company': get_default_company()
+        })
+        jv.append("accounts", {
+            'account': self.expense_account,
+            'debit_in_account_currency': (self.amount - self.vst),
+        })
+        if self.vst != 0:
+            jv.append("accounts", {
+                'account': self.expense_account,
+                'debit_in_account_currency': self.vst,
+            })
+        mode_of_payment = frappe.get_doc("Mode of Payment", self.payment)
+        account = None
+        for a in mode_of_payment.accounts:
+            if a.company == get_default_company():
+                account = a.default_account
+                
+        jv.append("accounts", {
+            'account': account,
+            'credit_in_account_currency': self.amount,
+        })
+        jv.insert()
+        jv.submit()
+        self.journal_entry = jv.name
+        self.save()
+        frappe.db.commit()
+        return
+        
+    def on_cancel(self):
+        if self.journal_entry:
+            jv = frappe.get_doc("Journal Entry", self.journal_entry)
+            if jv.docstatus == 1:
+                jv.cancel()
+                frappe.db.commit()
+        return
+        
+    
