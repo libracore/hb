7acab9997bbf1e6d3b2e61de8736e3566aab9c03
Ivanios <ivan.lochbihler@libracore.com>
2024-01-05 16:50:33 +0100

    getting projects in period

--- heimbohrtechnik/templates/pages/feedback_bohrmeter.js
+++ heimbohrtechnik/templates/pages/feedback_bohrmeter.js
@@ -8,7 +8,9 @@ function make() {
 }
 
 function run() {
-    $(".btn-submit").on('click', function() { 
+    //Create Document on submit
+    $(".btn-submit").on('click', function() {
+        var link_key = get_link_key()
         frappe.call({
             'method': 'heimbohrtechnik.templates.pages.feedback_bohrmeter.insert_feedback',
             'args': {
@@ -16,7 +18,8 @@ function run() {
                 'drilling_meter': document.getElementById('drilling_meter').value, 
                 'date': document.getElementById('date').value, 
                 'project': document.getElementById('project').value, 
-                'project2': document.getElementById('project2').value
+                'project2': document.getElementById('project2').value,
+                'link_key': link_key
             },
             'callback': function(r) {
             }
@@ -25,12 +28,13 @@ function run() {
     // Autofill Drilling Team
     var drilling_team_prep = window.location.toString().split("=");
     var drilling_team_prep_2 = drilling_team_prep[1].split("&");
-    var drilling_team = drilling_team_prep_2[0].replace(/%20/g, " ");
+    var drilling_team = drilling_team_prep_2[0].replace(/%20/g, " ").replace(/%C3%BC/g, "ü").replace(/%C3%B6/g, "ö").replace(/%C3%A4/g, "ä");
     document.getElementById('drilling_team').value = drilling_team;
     // Handle button visibillity
     // Validate key
-    var link_prep = window.location.toString().split("=");
-    var link_key = link_prep[link_prep.length -1]
+    var link_key = get_link_key()
+    //~ var link_prep = window.location.toString().split("=");
+    //~ var link_key = link_prep[link_prep.length -1]
     frappe.call({
         'method': 'heimbohrtechnik.templates.pages.feedback_bohrmeter.check_key',
         'args': {
@@ -39,10 +43,19 @@ function run() {
         },
         'callback': function(response) {
             if (response.message) {
-                var check = response.message
+                console.log(response.message);
+                var check = response.message[0]
+                var projects = response.message[1]
+                console.log(check);
+                console.log(projects);
             } else {
                 var check = false
             }
+            //Set Projects as Options for Select Field
+            var project_icon = document.getElementById('project_icon');
+                project_icon.addEventListener('click', function() {
+                    chose_project(projects);
+                });
             //Check if all mandatory fields are filled
             var input = document.getElementById('form')
             var button = document.getElementById('submit')
@@ -59,4 +72,16 @@ function run() {
             });
         }
     });
+}
+
+function get_link_key() {
+    var link_prep = window.location.toString().split("=");
+    var link_key = link_prep[link_prep.length -1]
+    return link_key
+}
+
+function chose_project(projects) {
+    console.log("hoi");
+    //~ html = document.getElementById('msgprint')
+    //~ frappe.msgprint(frappe.render_template('heimbohrtechnik/templates/pages/msgprint.html'), "Projekt wählen");
 }
--- heimbohrtechnik/templates/pages/feedback_bohrmeter.py
+++ heimbohrtechnik/templates/pages/feedback_bohrmeter.py
@@ -7,41 +7,47 @@ import frappe
 from frappe import _
 import json
 import datetime
+from frappe.utils.data import getdate
 
 @frappe.whitelist(allow_guest=True)
-def insert_feedback(drilling_team, drilling_meter, date, project, project2):
-    # create new record
-    if not project2:
-        feedback = frappe.get_doc({
-            'doctype': 'Feedback Drilling Meter',
-            'drilling_team': drilling_team,
-            'drilling_meter': drilling_meter,
-            'date': date,
-            #Create subtable "layers"
-            "project": [{
-            "reference_doctype": "Feedback Drilling Meter Project",
-            "project_number": project
-                }]
-        })
+def insert_feedback(drilling_team, drilling_meter, date, project, project2, link_key):
+    #check key
+    team_key = frappe.db.get_value("Drilling Team", drilling_team, "team_key")
+    if link_key != team_key:
+        return
     else:
-        feedback = frappe.get_doc({
-            'doctype': 'Feedback Drilling Meter',
-            'drilling_team': drilling_team,
-            'drilling_meter': drilling_meter,
-            'date': date,
-            #Create subtable "layers"
-            "project": [{
-            "reference_doctype": "Feedback Drilling Meter Project",
-            "project_number": project
-                },
-                {
+        # create new record
+        if not project2:
+            feedback = frappe.get_doc({
+                'doctype': 'Feedback Drilling Meter',
+                'drilling_team': drilling_team,
+                'drilling_meter': drilling_meter,
+                'date': date,
+                #Create subtable "layers"
+                "project": [{
                 "reference_doctype": "Feedback Drilling Meter Project",
-                "project_number": project2
-                }]
-        })
-    feedback = feedback.insert(ignore_permissions=True)
-    feedback.submit()
-    return {'feedback': feedback.name}
+                "project_number": project
+                    }]
+            })
+        else:
+            feedback = frappe.get_doc({
+                'doctype': 'Feedback Drilling Meter',
+                'drilling_team': drilling_team,
+                'drilling_meter': drilling_meter,
+                'date': date,
+                #Create subtable "layers"
+                "project": [{
+                "reference_doctype": "Feedback Drilling Meter Project",
+                "project_number": project
+                    },
+                    {
+                    "reference_doctype": "Feedback Drilling Meter Project",
+                    "project_number": project2
+                    }]
+            })
+        feedback = feedback.insert(ignore_permissions=True)
+        feedback.submit()
+        return {'feedback': feedback.name}
 
 @frappe.whitelist(allow_guest=True)
 def check_key(link_key, team):
@@ -52,7 +58,32 @@ def check_key(link_key, team):
     #validate key and prepare response
     if link_key == team_key:
         response = True
+        projects = get_projects(team)
     else:
-        response = False    
+        response = False  
+        projects = []  
     
-    return response
+    return response, projects
+
+def get_projects(team):
+    #get today and calculate start and end date of period
+    today = getdate()
+    period_start = frappe.utils.add_days(today, -5)
+    
+    #get projects in period
+    data = frappe.db.sql("""
+        SELECT `name`
+        FROM `tabProject`
+        WHERE ((`expected_start_date` >= '{ps}' AND `expected_start_date` <= '{pe}')
+        OR (`expected_end_date` >= '{ps}' AND `expected_end_date` <= '{pe}')
+        OR (`expected_start_date` < '{ps}' AND `expected_end_date` > '{pe}'))
+        AND `drilling_team` = '{team}'
+        AND `status` IN ("Open", "Completed")
+        """.format(team=team, ps=period_start, pe=today), as_dict=True)
+        
+    #get a list with the projects as values
+    projects = []
+    for project in data:
+          projects.append(project['name'])
+        
+    return projects
