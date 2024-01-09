fda11ffd547b2d208b862cdd53765da7257c55d6
Ivanios <ivan.lochbihler@libracore.com>
2024-01-09 11:40:29 +0100

    added day und week to doctype, created js file

---
+++ heimbohrtechnik/heim_bohrtechnik/doctype/feedback_drilling_meter_project/feedback_drilling_meter_project.js
@@ -0,0 +1,13 @@
+// Copyright (c) 2022, libracore AG and contributors
+// For license information, please see license.txt
+
+frappe.ui.form.on('Feedback Drilling Meter', {
+    before_save: function(frm) {
+        console.log("hoi");
+        autocomplete_day_and_week(frm);
+    }
+});
+
+function autocomplete_day_and_week(frm) {
+    console.log("hoi");
+}
