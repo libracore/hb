e0fc00b0448f438bd4a9d76c86dafe57823850b1
Ivanios <ivan.lochbihler@libracore.com>
2024-01-10 19:24:06 +0100

    collecting data

--- heimbohrtechnik/heim_bohrtechnik/report/feedback_drilling_meter/feedback_drilling_meter.py
+++ heimbohrtechnik/heim_bohrtechnik/report/feedback_drilling_meter/feedback_drilling_meter.py
@@ -31,7 +31,7 @@ def get_data(filters):
     # ~ start = datetime(int(filters.year_filter), 1, 1).date()
     # ~ end = datetime(int(filters.year_filter), 12, 31).date()
     
-    data =
+    data = []
     
     for i in range(1, 53):
         sql_query = """SELECT 
@@ -45,17 +45,36 @@ def get_data(filters):
             FROM `tabFeedback Drilling Meter`
             WHERE `drilling_team` = '{team}'
             AND `week` = '{i}'
-            """.format(i=i)
+            """.format(team=filters.drilling_team_filter, i=i)
             
-            entrys = frappe.db.sql(sql_query, as_dict=True)
+        entrys = frappe.db.sql(sql_query, as_dict=True)
+        
+        frappe.log_error(entrys, "entrys")
+        
+        new_week = {
+            'cw': i
+        }
+        
+        week_total = 0
+        remark = []
+        
+        for entry in entrys:
+            week_total += entry.drilling_meter
+            new_week[entry.day] = entry.drilling_meter
+            if entry.flushing == 1:
+                remark.append("Spülen")
+            if entry.hammer_change == 1:
+                remark.append("Hammerwechsel")
+            if entry.impact_part_change == 1:
+                remark.append("Schlagteilwechsel")
+            if entry.day == "Monday":
+                new_week['from'] = entry.date
+                new_week['to'] = frappe.utils.add_days(entry.date, 6)
+        
+        new_week['week'] = week_total
+        new_week['remark'] = remark
             
-            new_week = {
-                'cw' : i,
-                'from': xxx,
-                'to': xxx,
-                
-                
-
+        data.append(new_week)
     
     # ~ sql_query = """SELECT 
         # ~ `date`,
@@ -74,6 +93,6 @@ def get_data(filters):
     
     
     
-    # ~ frappe.log_error(weeks_in_year, "weeks_in_year")
+    frappe.log_error(data, "data")
         
     return
