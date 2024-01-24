83a62d7d2a0d8d15109f4008598b651fc28368bd
lasalesi <larsericmueller@gmail.com>
2024-01-24 23:56:39 +0100

    correct currency conversions

--- heimbohrtechnik/heim_bohrtechnik/doctype/expense_receipt/expense_receipt.js
+++ heimbohrtechnik/heim_bohrtechnik/doctype/expense_receipt/expense_receipt.js
@@ -5,14 +5,21 @@
 cur_frm.fields_dict['expense_account'].get_query = function(doc) {
      return {
          filters: {
-             "company": frappe.defaults.get_user_default("company") || frappe.defaults.get_global_default("company")
+             "company": cur_frm.doc.company || frappe.defaults.get_user_default("company") || frappe.defaults.get_global_default("company")
          }
      }
 }
 cur_frm.fields_dict['vat_account'].get_query = function(doc) {
      return {
          filters: {
-             "company": frappe.defaults.get_user_default("company") || frappe.defaults.get_global_default("company")
+             "company": cur_frm.doc.company || frappe.defaults.get_user_default("company") || frappe.defaults.get_global_default("company")
+         }
+     }
+}
+cur_frm.fields_dict['tax_template'].get_query = function(doc) {
+     return {
+         filters: {
+             "company": cur_frm.doc.company || frappe.defaults.get_user_default("company") || frappe.defaults.get_global_default("company")
          }
      }
 }
@@ -50,6 +57,30 @@ frappe.ui.form.on('Expense Receipt', {
                     
                     cur_frm.set_value("expense_account", settings.default_expense_account);
                     cur_frm.set_value("vat_account", settings.vat_account);
+                }
+            });
+        }
+        
+        if (!frm.doc.company) {
+            cur_frm.set_value("company", frappe.defaults.get_user_default("company") || frappe.defaults.get_global_default("company"));
+        }
+    },
+    tax_template: function(frm) {
+        if (frm.doc.tax_template) {
+            frappe.call({
+                'method': "frappe.client.get",
+                'args': {
+                    'doctype': "Purchase Taxes and Charges Template",
+                    'name': frm.doc.tax_template
+                },
+                'callback': function(response) {
+                    var tax_template = response.message;
+                    
+                    if (tax_template.taxes.length > 0) {
+                        var net_amount = 100 * frm.doc.amount / (100 + tax_template.taxes[0].rate);
+                        cur_frm.set_value("vst", frm.doc.amount - net_amount);
+                        cur_frm.set_value("vat_account", tax_template.taxes[0].account_head);
+                    }
                 }
             });
         }
--- heimbohrtechnik/heim_bohrtechnik/doctype/expense_receipt/expense_receipt.py
+++ heimbohrtechnik/heim_bohrtechnik/doctype/expense_receipt/expense_receipt.py
@@ -6,14 +6,19 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 from erpnext import get_default_company
+from erpnextswiss.erpnextswiss.finance import get_exchange_rate
+from frappe.utils import flt, rounded
 
 class ExpenseReceipt(Document):
         
     def on_submit(self):
         # create journal entry
         multi_currency = 0
-        if self.currency != "CHF":
+        exchange_rate = 1
+        company_currency = frappe.get_cached_value("Company", self.company, "default_currency")
+        if self.currency != company_currency:
             multi_currency = 1
+            exchange_rate = get_exchange_rate(from_currency=self.currency, company=self.company, date=self.date)
             
         jv = frappe.get_doc({
             'doctype': 'Journal Entry',
@@ -22,29 +27,60 @@ class ExpenseReceipt(Document):
             'user_remark': "{0} ({1})".format(
                 self.remarks or "Spesen {0}".format(self.employee_name),
                 self.name),
-            'company': get_default_company()
+            'company': self.company
         })
+        # expense allocation (company currency)
+        net_base_amount = rounded((self.amount - self.vst) * exchange_rate, 2)
         jv.append("accounts", {
             'account': self.expense_account,
-            'debit_in_account_currency': (self.amount - self.vst),
+            'debit_in_account_currency': net_base_amount,
+            'debit': net_base_amount,
+            'currency': company_currency
         })
+        # pretax allocation  (expense currency)
         if self.vst != 0:
+            pretax_base_amount = rounded(self.vst * exchange_rate, 2)
             jv.append("accounts", {
                 'account': self.vat_account,
                 'debit_in_account_currency': self.vst,
+                'debit': pretax_base_amount,
+                'exchange_rate': exchange_rate,
+                'account_currency': self.currency
             })
+            
+        # credit card allocation
         mode_of_payment = frappe.get_doc("Mode of Payment", self.payment)
         account = None
         for a in mode_of_payment.accounts:
-            if a.company == get_default_company():
+            if a.company == self.company:
                 account = a.default_account
-                
+        credit_card_currency = frappe.get_cached_value("Account", account, "account_currency")
+        base_gross_amount = rounded(self.amount * exchange_rate, 2)
         jv.append("accounts", {
             'account': account,
-            'credit_in_account_currency': self.amount,
+            'credit_in_account_currency': self.amount if credit_card_currency != company_currency else base_gross_amount,
+            'credit': base_gross_amount,
+            'exchange_rate': exchange_rate if credit_card_currency != company_currency else 1
         })
+        
+        # test for currency deviations
+        jv.set_total_debit_credit()
+        currency_deviation = rounded(jv.total_debit - jv.total_credit, 2)
+        if currency_deviation != 0:
+            currency_deviation_account = frappe.get_cached_value("Company", self.company, "exchange_gain_loss_account")
+            jv.append('accounts', {
+                'account': currency_deviation_account,
+                'credit': currency_deviation,
+                'account_currency': frappe.get_cached_value("Account", currency_deviation_account, "account_currency")
+            })
+
+            jv.set_total_debit_credit()
+        # insert and submit
+        jv.flags.ignore_validate = True
         jv.insert()
         jv.submit()
+
+
         self.journal_entry = jv.name
         self.save()
         frappe.db.commit()
