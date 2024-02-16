18a6fff74f8044d4d678385345e95a6fc5b2254a
lasalesi <larsericmueller@gmail.com>
2024-02-16 15:30:53 +0100

    updated debtors list

--- heimbohrtechnik/heim_bohrtechnik/report/forderungsliste/forderungsliste.py
+++ heimbohrtechnik/heim_bohrtechnik/report/forderungsliste/forderungsliste.py
@@ -3,7 +3,7 @@
 
 from __future__ import unicode_literals
 import frappe
-from datetime import datetime
+from datetime import datetime, timedelta
 from frappe import _
 from erpnext.accounts.report.accounts_receivable.accounts_receivable import ReceivablePayableReport
 
@@ -21,7 +21,9 @@ def get_columns(filters):
         {'fieldname': "outstanding", 'label': _("Outstanding Amount"), 'fieldtype': "Float", 'precision': 2, 'width': 80},
         {'fieldname': "currency", 'label': _("Currency"), 'fieldtype': "Data", 'width': 50},
         {'fieldname': "posting_date", 'label': _("Posting Date"), 'fieldtype': "Date", 'width': 80},
-        {'fieldname': "due_date", 'label': _("Due Date"), 'fieldtype': "Date", 'width': 80}
+        {'fieldname': "due_date", 'label': _("Due Date"), 'fieldtype': "Data", 'width': 80},
+        {'fieldname': "reminder_level", 'label': _("Reminder Level"), 'fieldtype': "Int", 'width': 80},
+        {'fieldname': "handwerkerpfand", 'label': _("HWP"), 'fieldtype': "Check", 'width': 80}
     ]
     
 def get_data(filters):
@@ -38,4 +40,13 @@ def get_data(filters):
     
     columns, data, more, chart = ReceivablePayableReport(filters).run(args)
     
+    for d in data:
+        if d['voucher_type'] == "Sales Invoice":
+            d['reminder_level'] = frappe.get_value(d['voucher_type'], d['voucher_no'], "payment_reminder_level")
+            d['handwerkerpfand'] = frappe.get_value(d['voucher_type'], d['voucher_no'], "handwerkerpfand_eingetragen")
+            if d['due_date'] < (datetime.today() - timedelta(days=45)).date():
+                d['due_date'] = "<span style=\"color: red; \">{0}</span>".format(d['due_date'].strftime("%d.%m.%Y"))
+            else:
+                d['due_date'] = "{0}".format(d['due_date'].strftime("%d.%m.%Y"))
+                
     return data
