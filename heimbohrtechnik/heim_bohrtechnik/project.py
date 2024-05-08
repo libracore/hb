c3e3a7003811fb83b06a6c2ed2ba201fb01830d4
lasalesi <larsericmueller@gmail.com>
2024-05-08 14:06:46 +0200

    improved trough handling on drilling team switch

--- heimbohrtechnik/heim_bohrtechnik/project.py
+++ heimbohrtechnik/heim_bohrtechnik/project.py
@@ -5,6 +5,7 @@ import frappe
 from frappe import _
 from frappe.utils import get_url_to_form, cint
 from heimbohrtechnik.heim_bohrtechnik.utils import clone_attachments
+from erpnextswiss.scripts.crm_tools import get_primary_supplier_address, get_primary_supplier_contact
 
 own_trough_supplier = "L-04052"         # when switching to an internal trough team, use this internal trough
 mud_from_trough = "L-03749"             # if this supplier is set for the mud, do not override trough
@@ -35,9 +36,11 @@ def before_save(self, method):
                         set_internal_trough = -1        # reset in case trough from mud supplier
                 if set_internal_trough >= 0:
                     frappe.db.set_value("Object", self.object, "old_trough_supplier", self.checklist[set_internal_trough].supplier, update_modified = False)
-                    self.checklist[set_internal_trough].supplier = own_trough_supplier
-                    self.checklist[set_internal_trough].supplier_name = frappe.get_value("Supplier", own_trough_supplier, "supplier_name")
                     frappe.db.commit()
+                    self.checklist[set_internal_trough].supplier = own_trough_supplier
+                    supplier_name = frappe.get_value("Supplier", own_trough_supplier, "supplier_name")
+                    self.checklist[set_internal_trough].supplier_name = supplier_name
+                    update_object_address(self.object, "Mulde", own_trough_supplier, supplier_name)
         else:
             # verify if an external trough is used
             reset_trough_supplier = False
@@ -49,22 +52,13 @@ def before_save(self, method):
             if reset_trough_supplier:
                 # if possible, revert to external trough supplier
                 supplier = frappe.get_value("Object", self.object, "old_trough_supplier")
-                if not supplier:
-                    object_trough_supplier = frappe.db.sql("""
-                        SELECT `party`, `party_name`
-                        FROM `tabObject Address`
-                        WHERE `parent` = "{obj}"
-                          AND `address_type` = "Mulde"
-                          AND `party` IS NOT NULL
-                        """.format(obj=self.object), as_dict=True)
-                    if len(object_trough_supplier) > 0:
-                        supplier = object_trough_supplier[0]['party']
                 if supplier:
                     if self.checklist:
                         for c in self.checklist:
                             if c.activity == "Mulde":
                                 c.supplier = supplier
                                 c.supplier_name = frappe.get_value("Supplier", supplier, "supplier_name")
+                                update_object_address(self.object, "Mulde", c.supplier, c.supplier_name)
     
     # bugfix: prevent 0 to null conversions
     if cint(self.actual_time) == 0:
@@ -77,6 +71,53 @@ def before_save(self, method):
         self.total_billable_amount = 0
         
     return
+
+def update_object_address(object_name, address_type, supplier, supplier_name):
+    address_doc = get_primary_supplier_address(supplier)
+    contact_doc = get_primary_supplier_contact(supplier)
+    frappe.db.sql("""
+        UPDATE `tabObject Address`
+        SET 
+            `party` = "{supplier}",
+            `party_name` = "{supplier_name}",
+            `address` = "{address}",
+            `contact` = "{contact}",
+            `address_display` = "{address_display}",
+            `contact_name` = "{contact_name}",
+            `phone` = "{phone}",
+            `email` = "{email}"
+        WHERE
+            `parent` = "{object_name}"
+            AND `parenttype` = "Object"
+            AND `address_type` = "{address_type}"
+            AND `party` != "{supplier}"     /* restrict: only update */
+        ;
+        """.format(
+            supplier=supplier, 
+            supplier_name=supplier_name,
+            address=address_doc.name if address_doc else "",
+            contact=contact_doc.name if contact_doc else "",
+            address_display=get_address_display(address_doc) if address_doc else "",
+            contact_name="{0} {1}".format(contact_doc.first_name or "", contact_doc.first_name or "") if contact_doc else "",
+            phone=contact_doc.phone if contact_doc else "",
+            email=contact_doc.email_id if contact_doc else "",
+            object_name=object_name,
+            address_type=address_type
+        )
+    )
+    return
+            
+def get_address_display(address):
+    if type(address) == str:
+        address = frappe.get_doc("Address", address)
+        
+    template = frappe.get_all("Address Template", filters={'is_default': 1}, fields=['name', 'template'])
+    if not template or len(template) == 0:
+        return None
+        
+    address_display = frappe.render_template(template[0]['template'], address.as_dict())
+    
+    return address_display
     
 @frappe.whitelist()
 def split_project(project):
