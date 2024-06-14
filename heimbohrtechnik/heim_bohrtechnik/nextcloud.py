f2d1dcb1357e7774470a27e5524b0631d724f779
lasalesi <larsericmueller@gmail.com>
2024-06-14 15:21:29 +0200

    water supply registration to nextcloud

--- heimbohrtechnik/heim_bohrtechnik/nextcloud.py
+++ heimbohrtechnik/heim_bohrtechnik/nextcloud.py
@@ -266,6 +266,12 @@ def upload_file(self, event):
             physical_file_name = get_physical_path(self.name)
             write_project_file_from_local_file (project, physical_file_name, PATHS['subprojects'])
             
+    elif self.attached_to_doctype == "Water Supply Registration":
+        project = frappe.get_value(self.attached_to_doctype, self.attached_to_name, "project")
+        if frappe.db.exists("Project", project):
+            physical_file_name = get_physical_path(self.name)
+            write_project_file_from_local_file (project, physical_file_name, PATHS['drilling'])
+            
     return
 
 """
