184fb46a0443299e5b8ec3a0012652e13aaa12dc
lasalesi <larsericmueller@gmail.com>
2024-06-21 11:07:00 +0200

    connect purchase receipt to nextcloud storage

--- heimbohrtechnik/heim_bohrtechnik/nextcloud.py
+++ heimbohrtechnik/heim_bohrtechnik/nextcloud.py
@@ -215,6 +215,15 @@ def upload_file(self, event):
             else:
                 write_project_file_from_local_file (project, physical_file_name, PATHS['supplier_other'])
     
+    elif self.attached_to_doctype == "Purchase Receipt":
+        project = frappe.get_value(self.attached_to_doctype, self.attached_to_name, "object")
+        if frappe.db.exists("Project", project):
+            physical_file_name = get_physical_path(self.name)
+            if frappe.get_value("Purchase Receipt", self.attached_to_name, "supplier") in ("L-80011", "L-80061", "L-80154"):
+                write_project_file_from_local_file (project, physical_file_name, PATHS['supplier_ews'])
+            else:
+                write_project_file_from_local_file (project, physical_file_name, PATHS['supplier_other'])
+                
     elif self.attached_to_doctype == "Quotation":
         project = frappe.get_value(self.attached_to_doctype, self.attached_to_name, "object")
         if frappe.db.exists("Project", project):
