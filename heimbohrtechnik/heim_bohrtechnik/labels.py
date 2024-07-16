a1cb478db0229ac610998f1e69a2b8da8735f279
zwieble <anja.friedrich@libracore.com>
2024-07-16 16:00:52 +0200

    change name of variables

--- heimbohrtechnik/heim_bohrtechnik/labels.py
+++ heimbohrtechnik/heim_bohrtechnik/labels.py
@@ -3,17 +3,20 @@ from frappe import _
 
 
 @frappe.whitelist()
-def get_article_label(item, label_type):
+def get_label(item, label_type):
     #get label printer
     settings = frappe.get_doc("Heim Settings", "Heim Settings")
     if label_type == "small":
-        if not settings.article_label_printer_small:
+        if not settings.item_label_printer_small:
             frappe.throw(_("Please define a label printer for small article labels under Heim Settings"))
-        label_printer = settings.article_label_printer_small
+        label_printer = settings.item_label_printer_small
+        label_template = "item_label_small.html"
     elif label_type == "big":
-        if not settings.article_label_printer_big:
+        if not settings.item_label_printer_big:
             frappe.throw(_("Please define a label printer for big article labels under Heim Settings"))
-        label_printer = settings.article_label_printer_big
+        label_printer = settings.item_label_printer_big
+        label_template = "item_label_big.html"
+
 
     #get raw data
     item = frappe.get_doc("Item", item)
@@ -22,11 +25,10 @@ def get_article_label(item, label_type):
         "item_code": item.item_code,
         "item_name": item.item_name,
         "price": item.standard_rate,
-        "barcode": item.barcode,
     }
 
     #prepare content
-    content = frappe.render_template("heim_bohrtechnik/templates/labels/price_label.html", data)
+    content = frappe.render_template("heim_bohrtechnik/templates/labels/"+label_template, data)
     #create label
     printer=frappe.get_doc("Label Printer", label_printer)
     pdf = create_pdf(printer, content)
