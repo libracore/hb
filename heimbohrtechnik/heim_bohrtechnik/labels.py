2dfd654775e260384ac035c9ebe27db9494784f1
zwieble <anja.friedrich@libracore.com>
2024-07-16 15:51:42 +0200

    first draft python

---
+++ heimbohrtechnik/heim_bohrtechnik/labels.py
@@ -0,0 +1,37 @@
+import frappe
+from frappe import _
+
+
+@frappe.whitelist()
+def get_article_label(item, label_type):
+    #get label printer
+    settings = frappe.get_doc("Heim Settings", "Heim Settings")
+    if label_type == "small":
+        if not settings.article_label_printer_small:
+            frappe.throw(_("Please define a label printer for small article labels under Heim Settings"))
+        label_printer = settings.article_label_printer_small
+    elif label_type == "big":
+        if not settings.article_label_printer_big:
+            frappe.throw(_("Please define a label printer for big article labels under Heim Settings"))
+        label_printer = settings.article_label_printer_big
+
+    #get raw data
+    item = frappe.get_doc("Item", item)
+
+    data = {
+        "item_code": item.item_code,
+        "item_name": item.item_name,
+        "price": item.standard_rate,
+        "barcode": item.barcode,
+    }
+
+    #prepare content
+    content = frappe.render_template("heim_bohrtechnik/templates/labels/price_label.html", data)
+    #create label
+    printer=frappe.get_doc("Label Printer", label_printer)
+    pdf = create_pdf(printer, content)
+
+    #return download
+    frappe.local.response.filename = "{name}.pdf".format(name=label_printer.replace(" ", "-").replace("/", "-"))
+    frappe.local.response.filecontent = pdf
+    frappe.local.response.type = "download"
