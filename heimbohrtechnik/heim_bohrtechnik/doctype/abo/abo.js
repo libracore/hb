52bf74809cbd6bed83a69c6c37dd71a72357538d
libracore <Gokuflynn>
2024-10-22 16:50:49 +0200

    added invoice and emails

--- heimbohrtechnik/heim_bohrtechnik/doctype/abo/abo.js
+++ heimbohrtechnik/heim_bohrtechnik/doctype/abo/abo.js
@@ -4,6 +4,33 @@
 frappe.ui.form.on('Abo', {
 	refresh: function(frm) {
         set_next_reminder_property(frm.doc.set_reminder_manually);
+        
+        if (!frm.doc.__islocal) {
+            cur_frm.set_df_property('reminder_date', 'hidden', 1);
+            frm.add_custom_button(__("Create Invoice"),  function(){
+              create_invoice(frm);
+            });
+        }
+        
+        // libracore E-Mail Composer
+        if (document.getElementsByClassName("fa-envelope-o").length === 0) {
+            cur_frm.page.add_action_icon(__("fa fa-envelope-o"), function() {
+                custom_mail_dialog(frm);
+            });
+            var target ="span[data-label='" + __("Email") + "']";
+            $(target).parent().parent().remove();
+        }
+        
+        if (frm.doc.customer) {
+            frm.set_query('contact', function() {
+                return {
+                    filters: {
+                        'link_doctype': 'Customer',
+                        'link_name': frm.doc.customer
+                    }
+                };
+            });
+        }
 	},
     set_reminder_manually: function(frm) {
         set_next_reminder_property(frm.doc.set_reminder_manually);
@@ -17,7 +44,36 @@ frappe.ui.form.on('Abo', {
         }
     },
     interval: function(frm) {
-        set_next_reminder(frm);
+        if (!frm.doc.__islocal) {
+            set_next_reminder(frm);
+        }
+    },
+    contact: function(frm) {
+        if (frm.doc.contact) {
+            frappe.call({
+                method: 'frappe.contacts.doctype.contact.contact.get_contact_details',
+                args: {
+                    contact: frm.doc.contact
+                },
+                callback: function(r) {
+                    if (r.message) {
+                        frm.set_value('contact_display', r.message.contact_display);
+                    }
+                }
+            });
+        }
+    },
+    customer: function(frm) {
+        if (frm.doc.customer) {
+            frm.set_query('contact', function() {
+                return {
+                    filters: {
+                        'link_doctype': 'Customer',
+                        'link_name': frm.doc.customer
+                    }
+                };
+            });
+        }
     }
 });
 
@@ -40,7 +96,7 @@ function set_end_date(disabled) {
 function set_next_reminder(frm) {
     if (!frm.doc.set_reminder_manually) {
         //set initial date
-        let initial_date = frm.doc.last_reminder || frm.doc.start_date
+        let initial_date = frm.doc.last_reminder || frm.doc.reminder_date
         //set years to add
         let years = 0
         if (frm.doc.interval == "Yearly") {
@@ -53,3 +109,39 @@ function set_next_reminder(frm) {
         cur_frm.set_value("next_reminder", next_reminder);
     }
 }
+
+function create_invoice(frm) {
+    frappe.call({
+        'method': 'heimbohrtechnik.heim_bohrtechnik.doctype.abo.abo.create_invoice',
+        'args': {
+            'doc_name': frm.doc.name
+        },
+        'callback': function(response) {
+            frappe.set_route("Form", "Sales Invoice", response.message);
+        }
+    });
+}
+
+
+function custom_mail_dialog(frm) {
+    frappe.call({
+        'method': 'heimbohrtechnik.heim_bohrtechnik.doctype.abo.abo.get_email_information',
+        'args': {
+            'contact': frm.doc.contact
+        },
+        'callback': function(response) {
+            var recipient = response.message.recipient;
+            var cc = response.message.cc;
+            new frappe.erpnextswiss.MailComposer({
+                doc: cur_frm.doc,
+                frm: cur_frm,
+                subject: "Abo " + cur_frm.doc.name,
+                recipients: recipient,
+                cc: cc,
+                'email_template': "Nachfrage Abo",
+                attach_document_print: false
+            });
+        }
+    });
+}
+
--- heimbohrtechnik/heim_bohrtechnik/doctype/abo/abo.py
+++ heimbohrtechnik/heim_bohrtechnik/doctype/abo/abo.py
@@ -6,6 +6,7 @@ from __future__ import unicode_literals
 import frappe
 from frappe.model.document import Document
 from frappe.utils import getdate, add_years
+import json
 
 class Abo(Document):
 	pass
@@ -13,7 +14,7 @@ class Abo(Document):
 def send_abo_reminder():
     #check if reminder should be sent
     if frappe.db.get_value("Heim Settings", "Heim Settings", "send_abo_reminder"):
-        print("0")
+        
         #get all abos with needed reminders
         abos_with_reminder = frappe.db.sql("""
                                             SELECT
@@ -25,7 +26,7 @@ def send_abo_reminder():
                                                 `next_reminder` <= CURDATE()
                                             AND
                                                 `disabled` = 0""", as_dict=True)
-        print(abos_with_reminder)
+        
         #send Reminder and update Abo Document
         for abo in abos_with_reminder:
             recipient = send_email_reminder(abo.get('name'), abo.get('customer_name'))
@@ -34,7 +35,6 @@ def send_abo_reminder():
     return
         
 def send_email_reminder(abo_name, customer_name):
-    print("1")
     #get recipient
     recipient = frappe.db.get_value("Heim Settings", "Heim Settings", "abo_reminder_to")
     
@@ -54,7 +54,6 @@ def send_email_reminder(abo_name, customer_name):
     return recipient
     
 def update_abo(abo_name, reminder_to):
-    print("2")
     #get Abo Doc
     abo_doc = frappe.get_doc("Abo", abo_name)
     
@@ -81,6 +80,62 @@ def update_abo(abo_name, reminder_to):
     abo_doc.save()
     frappe.db.commit()
     return
-
     
+@frappe.whitelist()
+def create_invoice(doc_name):
+    abo_doc = frappe.get_doc("Abo", doc_name)
     
+    # ~ #create sales invoice
+    sales_invoice_doc = frappe.get_doc({
+                                        'doctype': "Sales Invoice",
+                                        'customer' : abo_doc.get('customer'),
+                                        'object_description': abo_doc.get('description'),
+                                        'contact_person': abo_doc.get('contact')
+                                        })
+    
+    #add items to sales invoice
+    for item in abo_doc.items:
+        sales_invoice_doc.append("items", {
+                                        'reference_doctype': "Sales Invoice Item",
+                                        'item_code': item.get('item_code'),
+                                        'qty': item.get('qty')
+                                })
+    
+    sales_invoice_doc.save()
+    
+    #update abo
+    abo_doc.append("invoices", {
+                        'reference_doctype': "Abo Invoice",
+                        'invoice_date': getdate(),
+                        'invoice': sales_invoice_doc.name,
+                        'created_by': frappe.session.user
+                    })
+    
+    abo_doc.save()
+    frappe.db.commit()
+    
+    return sales_invoice_doc.name
+    
+@frappe.whitelist()
+def get_email_information(contact):
+    recipient = None
+    e_mails = frappe.db.sql("""
+                                SELECT
+                                    `email_id`,
+                                    `is_primary`
+                                FROM
+                                    `tabContact Email`
+                                WHERE
+                                    `parent` = '{contact}'
+                                ORDER BY
+                                    `is_primary` DESC""".format(contact=contact), as_dict=True)
+                                    
+    if len(e_mails) > 0:
+        recipient = e_mails[0].get('email_id')
+    
+    cc = frappe.session.user
+    
+    return {
+            'recipient' : recipient,
+            'cc': cc
+            }
