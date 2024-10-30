7c9797d1fe9ec622b1ad65ee606c8406191097c2
lasalesi <larsericmueller@gmail.com>
2024-10-30 21:48:50 +0100

    subcontracting order finish: pull probes

--- heimbohrtechnik/heim_bohrtechnik/doctype/subcontracting_order_finish/subcontracting_order_finish.js
+++ heimbohrtechnik/heim_bohrtechnik/doctype/subcontracting_order_finish/subcontracting_order_finish.js
@@ -3,7 +3,11 @@
 
 frappe.ui.form.on('Subcontracting Order Finish', {
     refresh: function(frm) {
-
+        frm.add_custom_button( __("Sonden holen"), function() 
+            {
+                fetch_probes(frm);
+            }
+        );
     },
     subcontracting_order: function(frm) {
         if (frm.doc.subcontracting_order) {
@@ -26,3 +30,44 @@ frappe.ui.form.on('Subcontracting Order Finish', {
         }
     }
 });
+
+function fetch_probes(frm) {
+    if (frm.doc.object) {
+        frappe.call({
+            "method": "frappe.client.get",
+            "args": {
+                "doctype": "Object",
+                "name": frm.doc.object
+            },
+            "callback": function(response) {
+                let obj = response.message;
+                let probes = [];
+                let id = 1;
+                for (let i = 0; i < obj.ews_specification.length; i++) {
+                    for (let j = 0; j < obj.ews_specification[i].ews_count; j++) {
+                        probes.push({
+                            'id': id,
+                            'depth': obj.ews_specification[i].ews_depth,
+                            'dimension': obj.ews_specification[i].ews_dimension,
+                            'pressure_level': obj.ews_specification[i].pressure_level,
+                            'probe_type': obj.ews_specification[i].probe_type,
+                        });
+                        id++;
+                    }
+                }
+                
+                add_probes(probes);
+            }
+        });
+    }
+}
+
+function add_probes(probes) {
+    cur_frm.clear_table("probes");
+    for (let i = 0; i < probes.length; i++) {
+        let child = cur_frm.add_child('probes');
+        frappe.model.set_value(child.doctype, child.name, 'id', probes[i].id);
+        frappe.model.set_value(child.doctype, child.name, 'depth', probes[i].depth);
+    }
+    cur_frm.refresh_field('probes');
+}
