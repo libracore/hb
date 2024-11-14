f662c0d92a9a07c6117d1c24340df33d55b44c60
lasalesi <larsericmueller@gmail.com>
2024-11-14 23:06:22 +0100

    enabled probe interface to hpt

--- heimbohrtechnik/hpt_solutions/doctype/probe/probe.py
+++ heimbohrtechnik/hpt_solutions/doctype/probe/probe.py
@@ -7,22 +7,47 @@ from frappe.model.document import Document
 import requests
 
 class Probe(Document):
-    def post_probe_to_hpt_cloud(self):
+    def before_save(self):
+        if self.name:
+            self.post_probe_to_hpt_cloud()
+        return
+        
+    def post_probe_to_hpt_cloud(self, debug=False):
         """
         Transmit this probe to the HPT cloud
         """
-        probe_dict = self.as_dict()
+        parameters = {
+            "probe": {
+                "name": self.name,
+                "naming_series": self.naming_series,
+                "manufacturer": self.manufacturer,
+                "product": self.product,
+                "probe_type": self.probe_type,
+                "disabled": self.disabled,
+                "outer_diameter": self.outer_diameter,
+                "pressure_level": self.pressure_level,
+                "nominative_pressure": self.nominative_pressure,
+                "wall_strength": self.nominative_pressure,
+                "radius": self.radius,
+                "inner_diameter": self.inner_diameter,
+                "volume_per_m": self.volume_per_m,
+                "material": self.material,
+                "max_short_inner_pressure": self.max_short_inner_pressure,
+                "tube_roughness": self.tube_roughness
+            },
+            "key": frappe.get_value("HPT Settings", "HPT Settings", "hpt_api_key")
+        }
         
         response = requests.post(
             "https://cloud.hpt.solutions/api/method/hptcloud.hptcloud.doctype.probe.probe.update_probe",
-            json = {
-                'probe': probe_dict,
-                'key': frappe.get_value("HPT Settings", "HPT Settings", "hpt_api_key")
-            }
+            json = parameters
         )
         
         if response.status_code != 200:
             frappe.log_error( "Probe upload failed with {0}".format(response.text), "HPT Upload failed")
-            
+        
+        if debug:
+            print(response.text)
+        
         return
     
