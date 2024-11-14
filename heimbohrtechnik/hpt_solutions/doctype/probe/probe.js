6fefd791e47a22de7587db18d8b6cb6f7e7dcb4e
Lars <lars.mueller@libracore.com>
2024-11-14 20:37:56 +0000

    Extend full probe structure

--- heimbohrtechnik/hpt_solutions/doctype/probe/probe.js
+++ heimbohrtechnik/hpt_solutions/doctype/probe/probe.js
@@ -2,7 +2,17 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Probe', {
-	// refresh: function(frm) {
-
-	// }
+    radius: function(frm) {
+        if (frm.doc.radius) {
+            // V = PI * (r^2) * h
+            let V_m3 = (frm.doc.radius ** 2) * Math.PI;
+            cur_frm.set_value("volume_per_m", V_m3); 
+        }
+    },
+    nominative_pressure: function(frm) {
+        if (frm.doc.nominative_pressure) {
+            cur_frm.set_value("max_short_inner_pressure", frm.doc.nominative_pressure * 1.5); 
+        }
+    }
 });
+
