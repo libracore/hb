8e36a9e1f11d6ec5864dc48c941e92fc9c6b9a26
zwieble <anja.friedrich@libracore.com>
2025-02-03 14:05:48 +0100

    map including teams

--- heimbohrtechnik/heim_bohrtechnik/page/teams_standorte/teams_standorte.js
+++ heimbohrtechnik/heim_bohrtechnik/page/teams_standorte/teams_standorte.js
@@ -45,19 +45,8 @@ frappe.teams_standorte = {
         var gps_long = 9.56121;
         var initial_zoom = 13;
         var geo = null;
-        var radius = 10;
         
-        // prepare various icons
-        var green_icon = new L.Icon({'iconUrl': '/assets/heimbohrtechnik/images/marker-icon-green.png'});
-        var red_icon = new L.Icon({'iconUrl': '/assets/heimbohrtechnik/images/BG_blau_48x48.png'});
-        var grey_icon = new L.Icon({'iconUrl': '/assets/heimbohrtechnik/images/marker-icon-grey.png'});
-        var blue_icon = new L.Icon({'iconUrl': '/assets/heimbohrtechnik/images/marker-icon.png'});
-        var orange_icon = new L.Icon({'iconUrl': '/assets/heimbohrtechnik/images/marker-icon-orange.png'});
-        var grey_icon_active = new L.Icon({'iconUrl': '/assets/heimbohrtechnik/images/marker-icon-grey_active.png'});
-        var green_icon_active = new L.Icon({'iconUrl': '/assets/heimbohrtechnik/images/marker-icon-green_active.png'});
-        var orange_icon_active = new L.Icon({'iconUrl': '/assets/heimbohrtechnik/images/marker-icon-orange_active.png'});
-        var blue_icon_active = new L.Icon({'iconUrl': '/assets/heimbohrtechnik/images/marker-icon_active.png'});
-        var green_icon_warning = new L.Icon({'iconUrl': '/assets/heimbohrtechnik/images/marker-icon-green_warning.png'});
+        var red_icon = new L.Icon({'iconUrl': '/assets/heimbohrtechnik/images/marker-icon-orange.png'});
         
         // create map     
         document.getElementById('map-container').innerHTML = "<div id='map' style='width: 100%; height: 800px;'></div>";
@@ -77,28 +66,20 @@ frappe.teams_standorte = {
                 if (r.message) {
                     geo = r.message;
                     if (geo) {
-                        console.log(geo);
-                        for (var i = 0; i < geo.environment.length; i++) {
+                        for (var i = 0; i < geo.length; i++) {
                             var icon = red_icon;
-                            L.marker([geo.environment[i].gps_lat, geo.environment[i].gps_long],
-                                {'icon': icon}).addTo(map)
-                                .bindPopup(get_popup_str(geo.environment[i].object, 
-                                    rate=geo.environment[i].rate,
-                                    sales_order=geo.environment[i].sales_order,
-                                    cloud_url=geo.environment[i].cloud_url,
-                                    sv=geo.environment[i].sv,
-                                    quotation=geo.environment[i].quotation,
-                                    to_depth=geo.environment[i].to_depth,
-                                    drilling_method=geo.environment[i].drilling_method,
-                                    arteser=geo.environment[i].arteser));
-
+                            if (geo[i].gps_coordinates){
+                                L.marker([geo[i].gps_coordinates.split(",")[0], geo[i].gps_coordinates.split(",")[1]],
+                                    {'icon': icon}).addTo(map)
+                                    .bindPopup(get_popup_str(geo[i].team_name, geo[i].project_name, geo[i].object_street, geo[i].object_location));
+                            }
                         }
                     }
                 }
                 // hack: issue a resize event
                 window.dispatchEvent(new Event('resize')); 
                 frappe.teams_standorte.end_wait();
-                frappe.show_alert(geo.environment.length + " Objekte geladen");
+                frappe.show_alert(geo.length + " Objekte geladen");
             }
         });
     },
@@ -110,75 +91,16 @@ frappe.teams_standorte = {
     }   
 }     
 
-function get_popup_str(object_name, rate=null, sales_order=null, cloud_url=null, sv=null, quotation=null, 
-    to_depth=null, drilling_method=null, arteser=null) {
-    html = "<b><a href=\"/desk#Form/Object/" 
-        + (object_name || "HB-AG") + "\" target=\"_blank\">" 
-        + (object_name || "HB-AG") + "</a></b>";
-    if (rate) {
-        html += "<br>CHF " + parseFloat(rate).toFixed(2);
+function get_popup_str(team_name, project_name=null, object_street=null, object_location=null) {
+    html = "<b>" + team_name + "</b>";
+    if (project_name) {
+        html += "<br>Projekt: <a href=\"/desk#Form/Project/" + project_name + "\" target=\"_blank\">" + project_name + "</a>";
     }
-    if (quotation) {
-        html += "<br><a href=\"/desk#Form/Quotation/" 
-        + quotation + "\" target=\"_blank\">" 
-        + quotation + "</a>";
+    if (object_street) {
+        html += "<br>Adresse: " + object_street;
     }
-    if (sales_order) {
-        html += "<br><a href=\"/desk#Form/Sales Order/" 
-        + sales_order + "\" target=\"_blank\">" 
-        + sales_order + "</a>";
-    }
-    if (cloud_url) {
-        html += "<br><a href=\"" + cloud_url + "\" target=\"_blank\"><i class=\"fa fa-cloud\"></i> Cloud</a>&nbsp;";
-    }
-    if (sv) {
-        html += "&nbsp;&middot;&nbsp;&nbsp;<a href=\"/desk#Form/Layer Directory/" 
-        + sv + "\" target=\"_blank\">SV</a>";
-    }
-    if (to_depth) {
-        html += "<br>Verrohrung bis " + to_depth + " m";
-    }
-    if (drilling_method) {
-        html += "<br>" + drilling_method;
-    }
-    if (arteser === 1) {
-        html += "<br><b>Vorsicht Arteser!</b>";
+    if (object_location) {
+        html += "<br>Ort: " + object_location;
     }
     return html;
-}
-
-function find_gps(address) {
-    if (!locals.gps_location_pending) {
-        locals.gps_location_pending = true;     // prevent multiple calls from plz/city change events
-        frappe.call({
-            'method': 'heimbohrtechnik.heim_bohrtechnik.doctype.object.object.get_gps',
-            'args': {
-                'street': address,
-                'location': ""
-            },
-            'callback': function(response) {
-                locals.gps_location_pending = false;
-                if (response.message) {
-                    if (response.message === "queued") {
-                        // the request has been sent, check back in a second
-                        setTimeout( function() {
-                            find_gps(address);
-                        }, 1000);
-                    } else {
-                        frappe.object_overview.render_map(address);
-                    }
-                } else {
-                    frappe.msgprint( __("Addresse nicht gefunden") , __("Geolocation") );
-                    frappe.object_overview.render_map();
-                }
-            }
-        });
-    }
-}
-
-function highlight_legend_item(class_name, weight) {
-    var legends = document.getElementsByClassName(class_name);
-    for (var i = 0; i < legends.length; i++) {
-        legends[i].style.fontWeight = weight;
-    }
 }
