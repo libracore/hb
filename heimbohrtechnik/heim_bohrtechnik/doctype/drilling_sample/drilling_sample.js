6925eb1d3b5df527b6370d9df5f19757cd6f8a3e
zwieble <anja.friedrich@libracore.com>
2025-02-12 10:42:44 +0100

    fetch data

--- heimbohrtechnik/heim_bohrtechnik/doctype/drilling_sample/drilling_sample.js
+++ heimbohrtechnik/heim_bohrtechnik/doctype/drilling_sample/drilling_sample.js
@@ -2,7 +2,40 @@
 // For license information, please see license.txt
 
 frappe.ui.form.on('Drilling Sample', {
-	// refresh: function(frm) {
-
-	// }
+	refresh: function(frm) {
+		toggle_drilling_depth(frm);
+	},
+	"project": function(frm) {
+		fetch_project_data(frm);
+	},
+	"drilling_samples_all": function(frm) {
+		toggle_drilling_depth(frm);
+	}
 });
+
+function fetch_project_data(frm) {
+	frappe.call({
+		method: 'heimbohrtechnik.heim_bohrtechnik.doctype.drilling_sample.drilling_sample.fetch_project_data',
+		args: {
+			"project_name": frm.doc.project
+		},
+		callback: function(response) {
+			var project_data = response.message;
+			if (project_data) {
+				console.log(project_data);
+				frm.set_value('object', project_data.object);
+				frm.set_value('address', project_data.address);
+				frm.set_value('geology_office_name', project_data.geology_office_name);
+				frm.set_value('geology_office', project_data.geology_office);
+			}
+		}
+	});
+}
+
+function toggle_drilling_depth(frm) {
+	if (frm.doc.drilling_samples_all == "benutzerdefiniert") {
+		frm.set_df_property('custom_drilling_depth', 'hidden', false);
+	} else {
+		frm.set_df_property('custom_drilling_depth', 'hidden', true);
+	}
+}
--- heimbohrtechnik/heim_bohrtechnik/doctype/drilling_sample/drilling_sample.py
+++ heimbohrtechnik/heim_bohrtechnik/doctype/drilling_sample/drilling_sample.py
@@ -3,8 +3,32 @@
 # For license information, please see license.txt
 
 from __future__ import unicode_literals
-# import frappe
+import frappe
+from frappe import _
 from frappe.model.document import Document
 
 class DrillingSample(Document):
 	pass
+
+@frappe.whitelist()
+def fetch_project_data(project_name):
+	project_data = {}
+	project = frappe.get_doc("Project", project_name)
+
+	object_data = frappe.get_doc("Object", project.object)
+	project_data["object"] = object_data.name
+
+	address = object_data.object_street + ", " + object_data.plz + " " + object_data.city
+	project_data["address"] = address
+
+	geology_office_addresses = object_data.addresses
+	for address in geology_office_addresses:
+		if address.address_type == "Geologe":
+			split_address = address.address_display.split("<br>")
+			if len(split_address) == 5:
+				address_string = split_address[1] + ", " + split_address[2] + ", " + split_address[3]
+			else:
+				address_string = split_address[1] + ", " + split_address[2]
+			project_data["geology_office"] = address_string
+			project_data["geology_office_name"] = split_address[0]
+	return project_data
