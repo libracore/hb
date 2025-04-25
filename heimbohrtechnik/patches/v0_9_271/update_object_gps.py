eb5118335ab72e4af2eba0fbf63544485abb3294
lasalesi <larsericmueller@gmail.com>
2025-04-25 08:48:08 +0200

    improved patch for GPS

--- heimbohrtechnik/patches/v0_9_271/update_object_gps.py
+++ heimbohrtechnik/patches/v0_9_271/update_object_gps.py
@@ -2,23 +2,22 @@
 # For license information, please see license.txt
 
 import frappe
+from frappe.utils import flt
+from tqdm import tqdm
 
 def execute():
     try:
-        loop = 1
         # Get all objects with GPS coordinates
         objects = frappe.get_all("Object", filters={"gps_coordinates": ["is", "set"]}, fields=["name", "gps_coordinates"])
         total= len(objects)
 
-        for obj in objects:
+        for obj in tqdm(objects, desc="Updating GPS", unit="object"):
             gps_coordinates = obj.gps_coordinates.split(",")
             if len(gps_coordinates) == 2:
-                lat = float(gps_coordinates[0].replace("'", ""))
-                long = float(gps_coordinates[1].replace("'", ""))
+                lat = flt(gps_coordinates[0].replace("'", ""))
+                lng = flt(gps_coordinates[1].replace("'", ""))
                 frappe.db.set_value("Object", obj.name, "gps_lat", lat)
-                frappe.db.set_value("Object", obj.name, "gps_long", long)
-            print("Updating GPS coordinates: {0}/{1}".format(loop, total))
-            loop += 1
+                frappe.db.set_value("Object", obj.name, "gps_long", lng)
         
         # Commit the changes
         frappe.db.commit()
